994da3ea20c61d6ef1b12dbab54149afce682739
marouen elaoud <97848633+marouenelaoud@users.noreply.github.com>
2022-10-02 13:20:44 +0100

    Add files via upload
    A simple calculator desktop application

---
+++ tp1/tp1/Form1.Designer.cs
@@ -0,0 +1,280 @@
+﻿namespace tp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.grpOp = new System.Windows.Forms.GroupBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtOp1 = new System.Windows.Forms.TextBox();
+            this.txtOp2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtExpression = new System.Windows.Forms.TextBox();
+            this.txtResultat = new System.Windows.Forms.TextBox();
+            this.cmdQuitter = new System.Windows.Forms.Button();
+            this.cmdInitialiser = new System.Windows.Forms.Button();
+            this.printPreviewControl1 = new System.Windows.Forms.PrintPreviewControl();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.grpOp.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(49, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "oprerande 1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(49, 157);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "operande 2 ";
+            // 
+            // grpOp
+            // 
+            this.grpOp.Controls.Add(this.button5);
+            this.grpOp.Controls.Add(this.button4);
+            this.grpOp.Controls.Add(this.button3);
+            this.grpOp.Controls.Add(this.button2);
+            this.grpOp.Controls.Add(this.button1);
+            this.grpOp.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.grpOp.ForeColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.grpOp.Location = new System.Drawing.Point(366, 26);
+            this.grpOp.Name = "grpOp";
+            this.grpOp.Size = new System.Drawing.Size(125, 258);
+            this.grpOp.TabIndex = 2;
+            this.grpOp.TabStop = false;
+            this.grpOp.Text = "operateur";
+            // 
+            // button5
+            // 
+            this.button5.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button5.Location = new System.Drawing.Point(49, 213);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(40, 22);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "%";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button4.Location = new System.Drawing.Point(49, 174);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(40, 24);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "/";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button3.Location = new System.Drawing.Point(51, 130);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(38, 24);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "*";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button2.Location = new System.Drawing.Point(49, 84);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(40, 25);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "-";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button1.Location = new System.Drawing.Point(49, 35);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(40, 24);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "+";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtOp1
+            // 
+            this.txtOp1.Location = new System.Drawing.Point(65, 97);
+            this.txtOp1.Name = "txtOp1";
+            this.txtOp1.Size = new System.Drawing.Size(164, 22);
+            this.txtOp1.TabIndex = 3;
+            this.txtOp1.TextChanged += new System.EventHandler(this.txtOp1_TextChanged);
+            // 
+            // txtOp2
+            // 
+            this.txtOp2.Location = new System.Drawing.Point(66, 185);
+            this.txtOp2.Name = "txtOp2";
+            this.txtOp2.Size = new System.Drawing.Size(163, 22);
+            this.txtOp2.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 282);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Exp :";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 333);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 16);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Res : ";
+            // 
+            // txtExpression
+            // 
+            this.txtExpression.Location = new System.Drawing.Point(65, 282);
+            this.txtExpression.Name = "txtExpression";
+            this.txtExpression.ReadOnly = true;
+            this.txtExpression.Size = new System.Drawing.Size(200, 22);
+            this.txtExpression.TabIndex = 7;
+            // 
+            // txtResultat
+            // 
+            this.txtResultat.Location = new System.Drawing.Point(66, 330);
+            this.txtResultat.Name = "txtResultat";
+            this.txtResultat.ReadOnly = true;
+            this.txtResultat.Size = new System.Drawing.Size(199, 22);
+            this.txtResultat.TabIndex = 8;
+            // 
+            // cmdQuitter
+            // 
+            this.cmdQuitter.BackColor = System.Drawing.Color.Red;
+            this.cmdQuitter.Location = new System.Drawing.Point(298, 397);
+            this.cmdQuitter.Name = "cmdQuitter";
+            this.cmdQuitter.Size = new System.Drawing.Size(111, 42);
+            this.cmdQuitter.TabIndex = 10;
+            this.cmdQuitter.Text = "Quitter";
+            this.cmdQuitter.UseVisualStyleBackColor = false;
+            this.cmdQuitter.Click += new System.EventHandler(this.cmdQuitter_Click);
+            // 
+            // cmdInitialiser
+            // 
+            this.cmdInitialiser.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.cmdInitialiser.Location = new System.Drawing.Point(97, 397);
+            this.cmdInitialiser.Name = "cmdInitialiser";
+            this.cmdInitialiser.Size = new System.Drawing.Size(111, 42);
+            this.cmdInitialiser.TabIndex = 11;
+            this.cmdInitialiser.Text = "Initialiser";
+            this.cmdInitialiser.UseVisualStyleBackColor = false;
+            this.cmdInitialiser.Click += new System.EventHandler(this.cmdInitialiser_Click);
+            // 
+            // printPreviewControl1
+            // 
+            this.printPreviewControl1.Location = new System.Drawing.Point(127, 507);
+            this.printPreviewControl1.Name = "printPreviewControl1";
+            this.printPreviewControl1.Size = new System.Drawing.Size(8, 8);
+            this.printPreviewControl1.TabIndex = 12;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Info;
+            this.ClientSize = new System.Drawing.Size(519, 473);
+            this.Controls.Add(this.printPreviewControl1);
+            this.Controls.Add(this.cmdInitialiser);
+            this.Controls.Add(this.cmdQuitter);
+            this.Controls.Add(this.txtResultat);
+            this.Controls.Add(this.txtExpression);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtOp2);
+            this.Controls.Add(this.txtOp1);
+            this.Controls.Add(this.grpOp);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "Form1";
+            this.Text = "Calculatrice";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.grpOp.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox grpOp;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtOp1;
+        private System.Windows.Forms.TextBox txtOp2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtExpression;
+        private System.Windows.Forms.TextBox txtResultat;
+        private System.Windows.Forms.Button cmdQuitter;
+        private System.Windows.Forms.Button cmdInitialiser;
+        private System.Windows.Forms.PrintPreviewControl printPreviewControl1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
---
+++ tp1/tp1/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (string.IsNullOrEmpty(txtOp1.Text) || string.IsNullOrEmpty(txtOp2.Text))
+                MessageBox.Show("attention :zone vide ! ");
+            else
+            {
+                double op1 = Convert.ToDouble(txtOp1.Text);
+                double op2 = Convert.ToDouble(txtOp2.Text);
+                double res = op1 + op2;
+                txtExpression.Text = txtOp1.Text + " + " + txtOp2.Text;
+                txtResultat.Text = res.ToString();
+            }
+
+
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtOp1.Text) || string.IsNullOrEmpty(txtOp2.Text))
+                MessageBox.Show("attention :zone vide ! ");
+            else
+            {
+                double op1 = Convert.ToDouble(txtOp1.Text);
+                double op2 = Convert.ToDouble(txtOp2.Text);
+                double res = op1 - op2;
+                txtExpression.Text = txtOp1.Text + " - " + txtOp2.Text;
+                txtResultat.Text = res.ToString();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtOp1.Text) || string.IsNullOrEmpty(txtOp2.Text))
+                MessageBox.Show("attention :zone vide ! ");
+            else
+            {
+                double op1 = Convert.ToDouble(txtOp1.Text);
+                double op2 = Convert.ToDouble(txtOp2.Text);
+                double res = op1 * op2;
+                txtExpression.Text = txtOp1.Text + " * " + txtOp2.Text;
+                txtResultat.Text = res.ToString();
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtOp1.Text) || string.IsNullOrEmpty(txtOp2.Text))
+                MessageBox.Show("attention :zone vide ! ");
+            else
+            {
+                double op1 = Convert.ToDouble(txtOp1.Text);
+                double op2 = Convert.ToDouble(txtOp2.Text);
+                double res = op1 / op2;
+                txtExpression.Text = txtOp1.Text + " / " + txtOp2.Text;
+                txtResultat.Text = res.ToString();
+            }
+            if (txtOp2.Text == "0")
+            {
+                txtResultat.Text = "Erreur";
+                MessageBox.Show(" La divison par 0 est impossible ! ");
+               
+
+            }
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtOp1.Text) || string.IsNullOrEmpty(txtOp2.Text))
+                MessageBox.Show("attention : zone vide !!! ");
+            else
+            {
+                double op1 = Convert.ToDouble(txtOp1.Text);
+                double op2 = Convert.ToDouble(txtOp2.Text);
+                double res = op1 % op2;
+                txtExpression.Text = txtOp1.Text + " % " + txtOp2.Text;
+                txtResultat.Text = res.ToString();
+            }
+        }
+
+        private void txtOp1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+
+        }
+
+        private void cmdQuitter_Click(object sender, EventArgs e)
+        {
+            this .Close();
+        }
+
+        private void cmdInitialiser_Click(object sender, EventArgs e)
+        {
+            txtOp1.Clear();
+            txtOp2.Clear(); 
+            txtResultat.Clear();
+            txtExpression.Clear();
+        }
+    }
+}
